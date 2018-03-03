47081e84fd466d2e01a3386ba4cfbfb479235256
Cristian Tinôco Tosta dos Reis <30469556+Psidom@users.noreply.github.com>
2018-03-03 01:51:09 -0300

    Ajuste nas regras de dos atributos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,11 @@ export class AppComponent {
 
   nome: '';
     idade: '';
-    atributo1: null;
-    atributo2: null;
-    atributo3: null;
-    atributo4: null;
-    atributo5: null;
+    atributo1 = null;
+    atributo2 = null;
+    atributo3 = null;
+    atributo4 = null;
+    atributo5 = null;
   datadia = new Date().toLocaleDateString('pt-BR');
 
   add() {
@@ -25,6 +25,17 @@ export class AppComponent {
     this.tasks.push(this.nome);
     this.tasks.push(this.idade);
     this.tasks.push(this.datadia);
+    if (this.atributo1 != null || this.atributo1 === 'true') {
+      this.atributo1 = 'Raio Laser';
+    }if (this.atributo2 != null || this.atributo2 === 'true') {
+      this.atributo2 = 'Braço Mecânico';
+    }if (this.atributo3 != null || this.atributo3 === 'true') {
+      this.atributo3 = 'Esqueleto Reforçado';
+    }if (this.atributo4 != null || this.atributo4 === 'true') {
+      this.atributo4 = 'Sentidos Aguçados';
+    }if (this.atributo5 != null || this.atributo5 === 'true') {
+      this.atributo5 = 'Pele Adaptativa';
+    }
     this.tasks.push(this.atributo1);
     this.tasks.push(this.atributo2);
     this.tasks.push(this.atributo3);
