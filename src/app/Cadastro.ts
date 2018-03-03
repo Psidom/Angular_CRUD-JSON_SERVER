f0c40380fecebd1ec607635a46eeceba77bd47b0
Cristian Tinôco Tosta dos Reis <30469556+Psidom@users.noreply.github.com>
2018-03-03 01:43:46 -0300

    Testes de tabela e javascript

---
+++ src/app/Cadastro.ts
@@ -0,0 +1,9 @@
+export class Cadastro {
+  nome: string;
+  idade: string;
+  atributo1: string;
+  atributo2: string;
+  atributo3: string;
+  atributo4: string;
+  atributo5: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { isDate } from 'util';
+import { getLocaleDateFormat, getLocaleDateTimeFormat, DatePipe } from '@angular/common';
+import { Cadastro } from './Cadastro';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  tasks = [];
+
+  nome: '';
+    idade: '';
+    atributo1: null;
+    atributo2: null;
+    atributo3: null;
+    atributo4: null;
+    atributo5: null;
+  datadia = new Date().toLocaleDateString('pt-BR');
+
+  add() {
+
+    this.tasks.push(this.nome);
+    this.tasks.push(this.idade);
+    this.tasks.push(this.datadia);
+    this.tasks.push(this.atributo1);
+    this.tasks.push(this.atributo2);
+    this.tasks.push(this.atributo3);
+    this.tasks.push(this.atributo4);
+    this.tasks.push(this.atributo5);
+  }
 }
