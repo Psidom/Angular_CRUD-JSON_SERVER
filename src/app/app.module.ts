a30afda16c65baeec5135734299814348693ba07
Cristian Tinôco Tosta dos Reis <30469556+Psidom@users.noreply.github.com>
2018-03-03 17:43:40 -0300

    Criação de Componente e adição de paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
+import {Routes, RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { getLocaleDateTimeFormat } from '@angular/common';
 import { AppComponent } from './app.component';
 import { MyRulesDirective } from './my-rules.directive';
+import { PostListComponent } from './post-list/post-list.component';
 
-
+const appRoutes: Routes = [
+  {path: 'posts', component: PostListComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    MyRulesDirective
+    MyRulesDirective,
+    PostListComponent
   ],
   imports: [
     FormsModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
