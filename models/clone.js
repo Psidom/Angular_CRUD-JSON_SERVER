d9eca26d33a203f73533df97886f9359beec4ce9
Cristian Tinôco Tosta dos Reis <30469556+Psidom@users.noreply.github.com>
2018-03-04 17:15:34 -0300

    Criação de Servidor

---
+++ models/clone.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var CloneSchema = new mongoose.Schema({
+  id: Number,
+  nome: String,
+  idade: String,
+  data: { type: Date, default: Date.now },
+  atributo1: String,
+  atributo2: String,
+  atributo3: String,
+  atributo4: String,
+  atributo5: String,
+});
+module.exports = mongoose.model('Clone', CloneSchema);
